d5a8487d943758dc20c7ae5e8c874aca8e4c2348
arkadijsyckov <Z4mmer@gmail.com>
2025-04-17 01:43:22 +0500

    complete selene test

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -1,18 +1,14 @@
 from selene.support import by
-from selene.support.conditions import be
+from selene.support.conditions import be, have
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
-
 
 def test_github():
     browser.open("https://github.com")
 
-    s(".header-search-input").click()
-    s(".header-search-input").send_keys("eroshenkoam/allure-example")
-    s(".header-search-input").submit()
-
-    s(by.link_text("eroshenkoam/allure-example")).click()
-
+    s('.search-input-container').click()
+    s('#query-builder-test').send_keys("eroshenkoam/allure-example")
+    s('#query-builder-test').submit()
+    s(by.link_text("eroshenkoam/allure-playwright-example")).click()
     s("#issues-tab").click()
-
-    s(by.partial_text("#76")).should(be.visible)
+    s('span.issue-item-module__defaultNumberDescription--GXzri > span').should(have.text("#1"))
